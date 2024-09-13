201b79396ed05dcd817f3e96177af6b8db6db27b
ladasedlacek <sedlacek.lada@gmail.com>
2024-09-13 10:55:02 +0200

    Fixed navigation bugs

--- js/_navigation.js
+++ js/_navigation.js
@@ -1,24 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('.navigation__link')
+    const navigation = document.querySelector('.navigation')
+    const header = document.querySelector('.header')
 
     // scrolling function
     const run_scroll = () => {
-        const navigation = document.querySelector('.navigation')
-        const offset_top = navigation.offsetTop
-        const header = document.querySelector('.header')
         const nav_height = navigation.offsetHeight
-    
-        // add sticky to the nav
-        const scrolling = () => {
-            if (window.scrollY > offset_top) {
-                navigation.classList.add('sticky')
-                header.style.marginTop = `${nav_height}px`
-            } else {
-                navigation.classList.remove('sticky')
-                header.style.marginTop = '0'
-            }
-        }
-        window.addEventListener('scroll', scrolling)
 
         // scroll to element
         const scroll_to = event => {
@@ -41,32 +28,32 @@ document.addEventListener('DOMContentLoaded', () => {
             link.addEventListener('click', scroll_to)
         })
     }
+    run_scroll()
     
-    // closing mobile navigation after user clicked on the link in navigation
-    const run_close = () => {
-        const menu_button = document.querySelector('.menu-button')
-        const navigation_itemsWrapper = document.querySelector('.navigation__itemsWrapper')
-        const links = document.querySelectorAll('.navigation__itemsWrapper a') 
-    
-        const handleClick = () => {
+    // toggle menu
+    const toggle_menu = () => {
+        const menu_button = document.querySelector('.menu-button');
+
+        const toggle = () => {
             if (window.innerWidth <= 1200) {
                 menu_button.checked = false
             }
-        }
-    
-        const handleOutsideClick = (event) => {
-            if (window.innerWidth <= 1200 && !navigation_itemsWrapper.contains(event.target) && !menu_button.contains(event.target)) {
-                menu_button.checked = false
+        };
+
+        // click out of the navigation - close menu
+        document.addEventListener('click', event => {
+            if (!navigation.contains(event.target) && !menu_button.contains(event.target)) {
+                toggle()
             }
-        }
-    
-        links.forEach(link => {
-            link.addEventListener('click', handleClick)
         })
-    
-        document.addEventListener('click', handleOutsideClick)
+
+        // link items click - close menu
+        links.forEach(link => {
+            link.addEventListener('click', event => {
+                event.stopPropagation()
+                toggle()
+            })
+        })
     }
-    
-    run_scroll()
-    run_close()
+    toggle_menu()
 })
